2400ad63935f46abbbd986e904d01a50ae33ca0f
Tublad <tublad92@gmail.com>
2021-04-21 14:28:30 +0600

    adding element for mock mediaSerivice

--- Tests/MediaServiceTests/MediaServiceTests.swift
+++ Tests/MediaServiceTests/MediaServiceTests.swift
@@ -1,12 +1,35 @@
 import XCTest
 @testable import MediaService
+import Ion
+import Photos
 
 final class MediaServiceTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MediaService().text, "Hello, World!")
+
+    lazy var service: MediaLibraryService = {
+        let service = MediaLibraryServiceImp()
+        service.mediaLibraryServiceTest = MediaLibraryServiceTestMock()
+        return service
+    }()
+
+    private lazy var permissionStatusCollector: Collector<PHAuthorizationStatus> = {
+        return .init(source: service.permissionStatusEventSource)
+    }()
+
+    private lazy var mediaItemCollectionsCollector: Collector<[MediaItemCollection]> = {
+        return .init(source: service.collectionsEventSource)
+    }()
+
+    private lazy var mediaItemResultCollector: Collector<MediaItemFetchResult> = {
+        return .init(source: service.mediaItemsEventSource)
+    }()
+
+    private lazy var mediaLibraryUpdateCollector: Collector<PHChange> = {
+        return .init(source: service.mediaLibraryUpdateEventSource)
+    }()
+
+    private lazy var mediaItemFetchProgressCollector: Collector<Float> = {
+        .init(source: service.mediaItemFetchProgressEventSource)
+    }()
     }
 
     static var allTests = [
