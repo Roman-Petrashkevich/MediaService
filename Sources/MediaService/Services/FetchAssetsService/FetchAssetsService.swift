557037b2a309135442567c08734e13498cf454c7
Tublad <tublad92@gmail.com>
2021-04-29 18:07:44 +0600

    add FetchThumbnailService

---
+++ Sources/MediaService/Services/FetchAssetsService/FetchAssetsService.swift
@@ -0,0 +1,37 @@
+//
+//  FetchAssetsService.swift
+//  Example
+//
+//  Created by Evgeny Schwarzkopf on 29.04.2021.
+//
+
+import Photos
+
+public protocol FetchAssetsService {
+    func fetchAssets(assetCollection: PHAssetCollection) -> PHAsset?
+    func makeAsset(item: MediaItem) -> PHAsset?
+}
+
+public class FetchAssetsServiceImp: FetchAssetsService {
+    public func fetchAssets(assetCollection: PHAssetCollection) -> PHAsset? {
+        if assetCollection.assetCollectionType == .smartAlbum ||
+            assetCollection.assetCollectionSubtype == .albumMyPhotoStream {
+            return PHAsset.fetchAssets(in: assetCollection, options: nil).lastObject
+        }
+        else {
+            return PHAsset.fetchAssets(in: assetCollection, options: nil).firstObject
+        }
+    }
+
+    public func makeAsset(item: MediaItem) -> PHAsset? {
+        if let asset = item.asset {
+            return asset
+        }
+
+        let fetchOptions = PHFetchOptions()
+        return PHAsset.fetchAssets(withLocalIdentifiers: [item.identifier], options: fetchOptions).firstObject
+    }
+
+    public init() {}
+}
+
