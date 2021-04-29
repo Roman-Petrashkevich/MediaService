b0dad32f19ae544b2811633c2575cad591909227
Tublad <tublad92@gmail.com>
2021-04-29 15:27:00 +0600

    add PermissionsService

---
+++ Sources/MediaService/Services/PermissionsService/PermissionsService.swift
@@ -0,0 +1,21 @@
+//
+//  PermissionsService.swift
+//  Example
+//
+//  Created by Evgeny Schwarzkopf on 28.04.2021.
+//
+
+import Photos
+
+public protocol PermissionsService {
+    func requestMediaLibraryPermissions(handler: @escaping (PHAuthorizationStatus) -> Void)
+}
+
+public class PermissionsServiceImp: PermissionsService {
+    public func requestMediaLibraryPermissions(handler: @escaping (PHAuthorizationStatus) -> Void) {
+        PHPhotoLibrary.requestAuthorization { (status: PHAuthorizationStatus) in
+            handler(status)
+        }
+    }
+}
+
