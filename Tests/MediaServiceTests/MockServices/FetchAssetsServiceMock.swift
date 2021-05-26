418b8942f9389b12d3b304acacd5491db41026df
Tublad <tublad92@gmail.com>
2021-05-27 01:56:30 +0600

    remove optional

--- Tests/MediaServiceTests/MockServices/FetchAssetsServiceMock.swift
+++ Tests/MediaServiceTests/MockServices/FetchAssetsServiceMock.swift
@@ -10,17 +10,14 @@ import Photos
 
 final class FetchAssetsServiceMock: FetchAssetsService {
 
-    var fetchAsset: PHAsset? = .init()
-    var makeAsset: PHAsset? = .init()
+    var fetchAsset: PHAsset = .init()
+    var makeAsset: PHAsset = .init()
 
     func fetchAssets(assetCollection: PHAssetCollection) -> PHAsset? {
-        print(#function)
-        return .init()
+        fetchAsset
     }
 
     func makeAsset(item: MediaItem) -> PHAsset? {
-        print(#function)
-        return .init()
+        makeAsset
     }
-
 }
