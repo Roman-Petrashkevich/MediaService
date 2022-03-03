9823d03386fd760e162fc9e3cb45232e7878f5d9
Tublad <tublad92@gmail.com>
2022-03-03 11:14:50 +0600

    update

--- Sources/MediaService/Extension/PHAssetCollection+Tools.swift
+++ Sources/MediaService/Extension/PHAssetCollection+Tools.swift
@@ -5,11 +5,11 @@
 import Photos
 
 extension PHAssetCollection {
-    var currentAssetItemsCount: Int {
+    public var currentAssetItemsCount: Int {
         currentAssetCount(.image) + currentAssetCount(.video) + currentAssetCount(.unknown) + currentAssetCount(.audio)
     }
 
-    func currentAssetCount(_ type: PHAssetMediaType) -> Int {
+    public func currentAssetCount(_ type: PHAssetMediaType) -> Int {
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format: "mediaType == %d", type.rawValue)
         let result = PHAsset.fetchAssets(in: self, options: fetchOptions)
